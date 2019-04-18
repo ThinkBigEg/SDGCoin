ee730f23efa2d4b00f471d067fa891dfd8f32f7d
AliTarek97 <32720110+AliTarek97@users.noreply.github.com>
2019-04-19 00:55:00 +0200

    Create user_to_hc.

--- contracts/loyality.sol
+++ contracts/loyality.sol
@@ -1,7 +1,25 @@
 pragma solidity ^0.4.0;
 
 contract loyality {
-    function loyality(){
+    /*function loyality(){
 
+    }*/
+    mapping (address => uint) userBalance;
+    mapping (address => uint) hcBalance;
+    mapping (address => bool) user;
+    mapping (address => bool) hc;
+
+    event Transfer(address from, address to, uint value);
+    modifier onlyUser(){
+        require(user[msg.sender] == true);
+    }
+
+    function user_to_hc(address hc_address , uint amount) public only_user(){
+        require(user_tokens[msg.sender] < amount);
+
+        user_tokens[msg.sender] -= amount;
+        hc_tokens[hc_address] +=amount;
+
+        emit Transfer(msg.sender , hc_address , amount);
     }
 }
