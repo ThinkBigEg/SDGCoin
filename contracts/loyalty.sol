859410d6c393b14b569251a301234cb9779b4cd4
AmrTarek <amrtarekmo@gmail.com>
2019-04-19 00:28:22 +0200

    create lp2user

---
+++ contracts/loyalty.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.4.0;
+
+contract loyalty {
+
+
+    mapping (address => bool) loyaltyProvider;
+    mapping (address => uint) lpBalance;
+    mapping (address => bool) user;
+    mapping (address => uint) userBalance;
+
+
+    event Transfer(address from , address to , uint amount);
+
+    modifier onlyLP(){
+        require(loyaltyProvider[msg.sender] == true);
+        _;
+    }
+
+
+    function lp2user(address userPK , string memory receipt_id, uint receipt_price ) public onlyLP{
+        require(user[userPK] == true);
+        lpBalance[msg.sender] += receipt_price;
+        userBalance[userPK] += receipt_price;
+
+        emit Transfer(msg.sender , userPK , receipt_price);
+    }
+
+
+
+}
