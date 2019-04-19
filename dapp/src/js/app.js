0f87c6cba5035566e1efd52688512666492c879d
AmrTarek <amrtarekmo@gmail.com>
2019-04-20 01:22:36 +0200

    Merge remote-tracking branch 'remotes/origin/dev-mirror' into dev + add 4 transaction fn "contract"

--- dapp/src/js/app.js
+++ dapp/src/js/app.js
@@ -7,17 +7,16 @@ App = { //app is equal to big object
         return App.initWeb3();
     },
 
-    initWeb3: function () {
-        // Is there an injected web3 instance?
-        if (typeof web3 !== 'undefined') {
-            App.web3Provider = web3.currentProvider;
-        } else {
-            // If no injected web3 instance is detected, fall back to Ganache
-            App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
-        }
-        web3 = new Web3(App.web3Provider);
+    initWeb3: function() {
 
-        return App.initContract(); //App.initContracts also works.
+        if (window.ethereum) {
+            const ethereum = window.ethereum
+            App.web3Provider = new Web3(ethereum)
+        }
+        ethereum.enable().then((account) => {
+            App.web3Provider.eth.defaultAccount = account[0]
+        });
+        return App.initContract();
     },
 
     initContract: function () {
@@ -26,8 +25,8 @@ App = { //app is equal to big object
             App.contracts.loyalty = TruffleContract(data);
 
             // Set the provider for our contract
-            App.contracts.loyalty.setProvider(App.web3Provider);
-
+            App.contracts.loyalty.setProvider(App.web3Provider.currentProvider);
+            App.contracts.loyalty.defaults({from: App.web3Provider.eth.defaultAccount});
             App.contracts.loyalty.deployed().then(function (instance) {
                 console.log("loyalty address : ", instance.address);
             });
@@ -38,19 +37,24 @@ App = { //app is equal to big object
 
     bindEvents: function () {
         $(document).on('click', '.btn-user2hc', App.user2hc);
+
+
+
     },
 
     user2hc: function(event){
         event.preventDefault();
+
         var hc_pk = document.getElementById("HCPK").value;
         var amount = document.getElementById("amount").value;
 
+        console.log("here")
         web3.eth.getAccounts(function (error, accounts) {
             if (error) {
                 console.log(error);
             }
+            console.log(accounts)
             var account = accounts[0];
-
             App.contracts.loyalty.deployed().then(function (instance) {
                 // Execute createIns as a transaction by sending account
                 instance.user2hc(hc_pk, amount, {from: account});
@@ -62,41 +66,95 @@ App = { //app is equal to big object
             }).catch(function (err) {
                 console.log(err.message);
             });
-            
         });
     },
 
-    changePage: function(){
-        var Institution_Address = document.getElementById("InstAddress").value;
-        localStorage.setItem('InstitutionAddress', Institution_Address);
-    },
+    lp2user: function(event){
+        event.preventDefault();
 
-    backtrackPage: function(){
-        App.contracts.Institution.getCreator(function (err,creator) {
-            if(!err)
-                localStorage.setItem('InstitutionAddress', creator);
+        var user_pk = document.getElementById("USRPK").value;
+        var amount = document.getElementById("amount").value;
+
+        console.log("here")
+        web3.eth.getAccounts(function (error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(accounts)
+            var account = accounts[0];
+            App.contracts.loyalty.deployed().then(function (instance) {
+                // Execute createIns as a transaction by sending account
+                instance.lp2user(user_pk, amount, {from: account});
+                instance.Transfer().watch(function (err, res) {
+                    if (!err) {
+                        console.log(res);
+                    }
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+            });
         });
     },
 
+    hc2fp: function(event){
+        event.preventDefault();
+
+        var fp_pk = document.getElementById("FPPK").value;
+        var amount = document.getElementById("amount").value;
+
+        console.log("here")
+        web3.eth.getAccounts(function (error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(accounts)
+            var account = accounts[0];
+            App.contracts.loyalty.deployed().then(function (instance) {
+                // Execute createIns as a transaction by sending account
+                instance.hc2fp(fp_pk, amount, {from: account});
+                instance.Transfer().watch(function (err, res) {
+                    if (!err) {
+                        console.log(res);
+                    }
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+            });
+        });
+    },
+
+    fp2lp: function(event){
+        event.preventDefault();
+
+        var lp_pk = document.getElementById("LPPK").value;
+        var amount = document.getElementById("amount").value;
+
+        console.log("here")
+        web3.eth.getAccounts(function (error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+            console.log(accounts)
+            var account = accounts[0];
+            App.contracts.loyalty.deployed().then(function (instance) {
+                // Execute createIns as a transaction by sending account
+                instance.fp2lp(lp_pk, amount, {from: account});
+                instance.Transfer().watch(function (err, res) {
+                    if (!err) {
+                        console.log(res);
+                    }
+                });
+            }).catch(function (err) {
+                console.log(err.message);
+            });
+        });
+    },
 
 };
 
 
 //what this means is whenever the server loads the window we want to initialize our app (App.initWeb3).
 $(document).ready(function () {
-   if (window.location.pathname === "/Course.html") {
-
-        $(function () {
-            App.initCoursePg();
-            console.log("App Course..");
-        });
-
-    }
-    else {
-        $(function () {
-            App.initWeb3();
-            console.log("App UserUI..");
-        });
-    }
-
+   App.init();
 });
+
--- dapp/truffle-config.js
+++ dapp/truffle-config.js
@@ -24,8 +24,14 @@ module.exports = {
   networks: {
     development: {
     host: "127.0.0.1",
-    port: 8545,
+    port:8545,
     network_id: "*" // Match any network id
    }
+  },
+  solc: {
+    optimizer: {
+      enabled: true,
+      runs: 200
+    }
   }
 };
